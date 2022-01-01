21de47af7e885dfdb5ed7675f308ceeda3c127ac
tiianprb <96933129+tiianprb@users.noreply.github.com>
2022-01-01 06:50:57 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "${{ secrets.bot_token }}"  # Your telegram bot token
-DEVELOPER_ID = int("${{ secrets.developer_id }}")  # Your telegram id (used for error reporting and private mode)
+BOT_TOKEN = "${{ secrets.5050150479:AAE3QJAgLW88isOmXbxfysK0sJFTgf0InDs }}"  # Your telegram bot token
+DEVELOPER_ID = int("${{ secrets.1892879990 }}")  # Your telegram id (used for error reporting and private mode)
 IS_BOT_PRIVATE = False  # Change this to True to make bot private
